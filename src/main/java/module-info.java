40a6586795661909cdc06f3c4b3924f996145449
vncharles <80318266+vncharles@users.noreply.github.com>
2023-06-27 22:17:23 +0700

    update version log4j

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,18 +1,16 @@
 module com.example.chatapp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
     requires com.rabbitmq.client;
     requires org.apache.commons.lang3;
     requires org.slf4j;
-    requires javafx.graphics;
 
     opens com.example.chatapp to javafx.fxml;
     exports com.example.chatapp;
     opens com.example.chatapp.controller to javafx.fxml;
     exports com.example.chatapp.controller;
-    opens com.example.chatapp.rabbitmq to javafx.fxml;
-    exports com.example.chatapp.rabbitmq;
 }
